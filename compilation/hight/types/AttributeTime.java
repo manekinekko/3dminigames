b622f327d4a07bc57a24c9a278ec099650b6786b
beben35170 <beben35170@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-01-19 14:45:52 +0000

    Modif type, ajout AttributeTime.java

---
+++ compilation/hight/types/AttributeTime.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package types;
+
+/**
+ *
+ * @author Quentin
+ */
+public class AttributeTime implements Attributes {
+    private int valeur;
+	private String unite;
+
+    public AttributeTime(int v,String unite) {
+        this.valeur = v;
+		this.unite = unite;
+    }
+
+    public int TimeValue() {
+        return valeur;
+    }
+	
+	public int TimeUnit(){
+		return unite;
+	}
+    @Override
+    public String toString() {
+        return valeur+unite;
+    }
+}

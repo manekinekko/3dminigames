51ff9ef29eb7a0384ba94a16789848c0e3b706f5
wassim.chegham <wassim.chegham@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-01-29 14:58:08 +0000

    * REORGANISATION TOTALE DES REPERTOIRES pour une meilleur organisation!

---
+++ dev/iteration1/bin/upload.php
@@ -0,0 +1,34 @@
+<?php
+
+
+ini_set("memory_limit","128M");
+//echo ini_get("memory_limit");
+
+// Destination folder for downloaded files
+$upload_folder = 'assets/dae/custom';
+
+// If the browser supports sendAsBinary () can use the array $ _FILES
+if(count($_FILES)>0) { 
+
+	if( move_uploaded_file( $_FILES['upload']['tmp_name'] , $upload_folder.'/'.$_FILES['upload']['name'] ) ) {
+		echo 'OK';
+	}
+	exit();
+	
+} else if(isset($_GET['up'])) {
+	
+	// If the browser does not support sendAsBinary ()
+	if(isset($_GET['base64'])) {
+		$content = base64_decode(file_get_contents('php://input'));
+	} else {
+		$content = file_get_contents('php://input');
+	}
+
+	
+	if(file_put_contents($upload_folder.'/'.$_GET['filename'], $content)) {
+		echo 'OK';
+	}
+		
+	exit();
+}
+?>
---
+++ dev/iteration1/bin/xml_gen.php
@@ -0,0 +1,92 @@
+<?php
+  function genXML($data) {
+	
+  	//print_r($data); exit();
+  	
+  	$camtruc = $data['cam'];
+  	$scene = $data['scene'];
+  	$tabcollada = $data['colladas']; 
+  	$tablight = $data['lights'];
+  	
+	$dom = new DomDocument;
+	
+	// append comment
+    $glge = $dom->createComment("\nAuto generated XML level for GLGE by 3DWIGS on ". date('c', time())."\n");
+    $dom->appendChild($glge);
+    
+    
+    // append root document
+    $glge = $dom->createElement("glge");
+    $dom->appendChild($glge);
+	
+    
+    // append camera
+    $cam_node = $dom->createElement("camera");
+	$cam_node->setAttribute("id", $camtruc['id']);
+	$cam_node->setAttribute("loc_x", (float)$camtruc['loc_x']);
+	$cam_node->setAttribute("loc_y", (float)$camtruc['loc_y']);
+	$cam_node->setAttribute("loc_z", (float)$camtruc['loc_z']);
+	$cam_node->setAttribute("rot_x", (float)$camtruc['rot_x']);
+	$cam_node->setAttribute("rot_y", (float)$camtruc['rot_y']);
+	$cam_node->setAttribute("rot_z", (float)$camtruc['rot_z']);
+	$glge->appendChild($cam_node);
+	
+	
+	// append the scene
+	$scene_node = $dom->createElement("scene");
+    $scene_node->setAttribute("id", $scene['id']);
+	$scene_node->setAttribute("camera", "#".$cam_node->getAttribute('id'));
+
+
+	// append collada models
+	foreach( $tabcollada as $i=>$collada ){
+		$col_node = $dom->createElement("collada");
+		$col_node->setAttribute("id", $collada['id']);
+		$col_node->setAttribute("document", $collada['document']);
+		$col_node->setAttribute("loc_x", (float)$collada['loc_X']);
+		$col_node->setAttribute("loc_y", (float)$collada['loc_Y']);
+		$col_node->setAttribute("loc_z", (float)$collada['loc_Z']);
+		$col_node->setAttribute("rot_x", (float)$collada['rot_X']);
+		$col_node->setAttribute("rot_y", (float)$collada['rot_Y']);
+		$col_node->setAttribute("rot_z", (float)$collada['rot_Z']);
+		$col_node->setAttribute("scale", (float)$collada['scale']);
+		if((bool)$collada['isDuplicable']==false){
+			$col_node->setAttribute("isDuplicable", "false");
+			$scene_node->appendChild($col_node);
+		}
+		else {
+			$col_node->setAttribute("isDuplicable", "true");
+			$glge->appendChild($col_node);
+		}
+	}
+
+
+
+	foreach( $tablight as $i=>$light ){
+		$light_node = $dom->createElement("light");
+		$light_node->setAttribute("id", $light['id']);
+		$light_node->setAttribute("loc_x", (float)$light['loc_X']);
+		$light_node->setAttribute("loc_y", (float)$light['loc_Y']);
+		$light_node->setAttribute("loc_z", (float)$light['loc_Z']);
+		$light_node->setAttribute("type", $light['type']);
+		$scene_node->appendChild($light_node);
+	}
+
+
+	$glge->appendChild($scene_node);
+	
+	$filename = "level-".time().".xml";
+	$dom->preserveWhiteSpace = false;
+	$dom->formatOutput = true;
+	$dom->save($filename);
+	return $filename;
+}	
+
+
+header('Content-Type: application/json');
+
+$data = json_decode(stripslashes($_POST['glge']), true); // true for assoc array, 3 for depth
+
+$name = genXML($data);
+echo json_encode(array('filename'=>$name));	
+?>

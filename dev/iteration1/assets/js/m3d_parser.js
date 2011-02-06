cd17586819fa582c0dd73ca5e6a30a8eb82ffdcb
wassim.chegham <wassim.chegham@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-02-06 22:52:30 +0000

    * add new grid

--- dev/iteration1/assets/js/m3d_parser.js
+++ dev/iteration1/assets/js/m3d_parser.js
@@ -1,3 +1,235 @@
 /**
  * @author CHEGHAM Wassim <wassim.chegham@gmail.com>
  */
+
+if(!window["M3D"]){
+	window["M3D"]={};	
+}
+
+if (!window["M3D"].PARSER){
+		window["M3D"].PARSER={};
+}
+
+(function(M3D){
+	
+ 
+	 var M3DSyntax = [
+	 
+		{
+		    regex:  /^(?:break|case|catch|continue|default|delete|do|else|false|finally|for|function|if|in|instanceof|let|new|null|return|switch|this|throw|true|try|typeof|var|void|while|with)(?![a-zA-Z0-9_])/,
+		    tag:    'keyword'
+	    },
+	    {
+		    regex:  /^[A-Za-z_][A-Za-z0-9_]*/,
+		    tag:    'identifier'
+	    },
+	    {
+		    regex:  /^[^'"/ \tA-Za-z0-9_]+/,
+		    tag:    'plain'
+	    },
+	    {
+		    regex:  /^[ \t]+/,
+		    tag:    'plain'
+	    },
+	    {
+		    regex:  /^'/,
+		    tag:    'string',
+		    then:   'qstring'
+	    },
+	    {
+		    regex:  /^"/,
+		    tag:    'string',
+		    then:   'qqstring'
+	    },
+	    {
+		    regex:  /^\/\/.*/,
+		    tag:    'comment'
+	    },
+	    {
+		    regex:  /^\/\*/,
+		    tag:    'comment',
+		    then:   'comment'
+	    },
+	    {
+	    	regex:  /^./,
+		   	tag:    'plain'
+		}
+	 ];
+ 
+ 
+ })(window["M3D"]);
+ 
+ /*****
+PD	: ')';
+PG	: '(';
+FIN	: ';' ; //op�rateur de fin de r�gles
+VIRG	: ',';
+HAS	: 'has';
+AT	: 'at';
+IS	: 'is';
+AND	: 'and';
+OF	: 'of';
+WITH	: 'with';
+BETWEEN	: 'between';
+PLUS	: '+';
+MINUS	: '-';
+MUL	: '*';
+DIV	: '/';
+POW	: '^';
+MOD	: '%';
+X	: 'x';
+Y	: 'y';
+Z	: 'z';
+ALL	: 'all';
+NOT	: 'not';
+MIN	: 'min';
+SEC	: 'sec';
+MS	: 'ms';
+THEN	: 'then';
+FOR	: 'for';
+EQUALS : '=';
+INF :  '<';
+SUP :  '>';
+INFEG : '<=';
+SUPED : '>=';
+DIFF : '!=';
+
+GAME		: 'Game';
+GRAVITY_KW	: 'gravity';
+SCORE		: 'score';
+TYPE		: 'type';
+PLAYER		: 'player';
+LIST_KW		: 'list';
+IN		: 'in';
+LOOP		: 'loop';
+ONCE		: 'once';
+RANDOM_KW	: 'random';
+DEFINITION_KW	: 'definition';
+MEANS		: 'means';
+PAUSE_KW	: 'pause';
+MUTE_KW		: 'mute';
+ON		: 'on';
+OFF		: 'off';
+PLAY_KW		: 'play';
+STOP_KW		: 'stop';
+ENDS_KW		: 'ends';
+STARTS_KW	: 'starts';
+BLOCK_KW	: 'block';
+TRANSLATION	: 'translation';
+ROTATION	: 'rotation';
+SCALE		: 'scale';
+EFFACE_KW	: 'efface';
+GENERATE_KW	: 'generate';
+WAIT_KW		: 'wait';
+ENDWAIT		: 'endWait';
+SAVE_KW		: 'save';
+RULE_KW		: 'rule';
+MOVES_KW : 'moves'; 
+DIES_KW : 'dies';
+TOUCHES_KW : 'touches';
+KILLS_KW : 'kills';
+KILLED_KW : 'killed';
+TOUCHED_KW : 'touched';
+BY : 'by';
+OTHER : 'other';
+BECOMES : 'becomes';
+VICTORY_KW : 'victory';
+DEFEAT_KW : 'defeat';
+IF_KW : 'if';
+ELSE : 'else';
+ENDIF : 'endIf';
+OR : 'or';
+COMP : 'comp'; 
+DEAD_KW : 'dead';
+ALIVE_KW : 'alive';
+EFFACED_KW :'effaced';
+GENERATED_KW : 'generated';
+TOUCHING_KW : 'touching';
+MOVING_KW : 'moving';
+WAITING_KW : 'waiting';
+FINISHED_KW : 'finished';
+STARTED_KW : 'started'; 
+PAUSED_KW : 'paused';  
+MUTED_KW : 'muted';
+PLAYED_KW : 'played';  
+STOPPED_KW : 'stopped';
+ASSIGN_KW : 'assign';
+ADD_KW : 'add';
+SUB_KW : 'sub';
+INVERT_KW : 'invert';
+NUM 		: 'num';
+IA_KW 	:	 'ia';
+DURING : 'during';
+UNTIL : 'until';
+EQUIP : 'equip';
+NEXT : 'next';
+PREVIOUS : 'previous';
+KEYBOARD : 'keyboard';
+ACTIVATE_KW : 'activate';
+DISABLE_KW:'disable';
+COMMANDS:'commands';
+
+DUPLICABLE	: 'duplicable';
+FIRST		: 'first';
+THIRD		: 'third';
+ALLY		: 'ally';
+ENEMY		: 'enemy';
+NEUTRAL		: 'neutral';
+POSITION	: 'position';
+ORIENTATION	: 'orientation';
+SIZE		: 'size';
+PERSON		: 'person';
+FREE		: 'free';
+FRAME		: 'frame' | 'frames';
+
+// Control
+WUP	: 'up';
+WDOWN	: 'down';
+LEFT	: 'left';
+RIGHT	: 'right';
+SPACE	: 'space';
+ESCAPE	: 'escape';
+ENTER	: 'enter';
+MOUSE	: 'mouse';
+KEY	: 'key';
+CLICK_LEFT	: 'lClick';
+CLICK_CENTER	: 'cClick';
+CLICK_RIGHT	: 'rClick';
+SCROLL_UP	: 'scrollUp';
+SCROLL_DOWN	: 'scrollDown';
+
+COMMAND_KW	: 'command';
+JUMP : 'jump';
+MOVE : 'move';
+FORWARD : 'forward';
+BACKWARD : 'backward';
+TURN : 'turn';
+ACCELERATE : 'accelerate';
+BRAKE : 'brake';
+
+
+CAMERA	: 'Camera';
+MEDIA	: 'Media';
+COUNTER	: 'Counter';
+TIME	: 'Time';
+
+VALUE		: 'value';
+
+BOOST_INTERVAL	: 'boostInterval';
+SHOOT_INTERVAL	: 'shootInterval';
+RELOAD_TIME	: 'reloadTime';
+
+INVENTORY	: 'inventory';
+EQUIPED_OBJECT	: 'equipedObjects';
+ENTRANCES	: 'entrances';
+EXITS		: 'exits';
+DAMAGE_ZONE	: 'damageZone';
+COLLECTORS	: 'collectors';
+TYPES_COLLECTORS: 'typesCollectors';
+GENERATORS	: 'generators';
+TYPE_GENERATORS : 'typeGenerators';
+BREAKERS	: 'breakers';
+TYPES_BREAKERS	: 'typesBreakers';
+TELEPORTABLES	: 'teleportables';
+TYPES_TELEPORTABLES :'typesTeleportables';
+  */

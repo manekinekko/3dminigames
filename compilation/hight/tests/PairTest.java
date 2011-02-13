dc308724e6045b13c83866f6557b5cefa93be9b4
mathieuguichaoua <mathieuguichaoua@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-02-13 18:19:31 +0000

    suite tests

--- compilation/hight/tests/PairTest.java
+++ compilation/hight/tests/PairTest.java
@@ -3,9 +3,24 @@ import lib.Pair;
 import junit.framework.TestCase;
 
 public class PairTest extends TestCase {
-
+Pair p1;
+Pair p2;
+Pair p3;
 	protected void setUp() throws Exception {
 		super.setUp();
+		p1 = new Pair(3,5);
+		p2 = new Pair(3,5);
+		p3 = new Pair(3,4);
 	}
-
+	public void testHashCode(){
+		assertNotNull(p1.hashCode());
+		assertEquals(p1.hashCode(),43);
+		assertNotNull(p2);
+		assertNotNull(p3);
+		assertEquals(p1.equals(p2),true);
+		assertEquals(p1.equals(p3),false);
+		assertEquals(p1.toString(),"(3, 5)");
+		assertEquals(p1.getFirst(),3);
+		assertEquals(p1.getSecond(),5);
+	}
 }

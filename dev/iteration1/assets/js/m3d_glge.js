8dececd58d133aa7d8f8871cac4837ffbc8bdf51
Eldius27 <Eldius27@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-02-16 12:33:14 +0000

    Correction IndexedDB + commentaires de m3d_db.js

--- dev/iteration1/assets/js/m3d_glge.js
+++ dev/iteration1/assets/js/m3d_glge.js
@@ -14,12 +14,8 @@ doc.onLoad = function() {
 	
 	// Initilization
 	M3D.GUI.init();
-
-	//local storage
-	if (M3D.DB.init()){ 
-		M3D.DB.init();
-		M3D.Editor.init();
-	}
+	M3D.DB.init();
+	M3D.Editor.init();
 	
 	// -- objet and camera rotation
 	controller.onchange = function(xRot, yRot) { 

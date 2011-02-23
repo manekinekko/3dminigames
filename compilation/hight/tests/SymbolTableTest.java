ca217f05b2ac855544e0fd95f2809f83f89d6428
mathieuguichaoua@gmail.com <mathieuguichaoua@gmail.com@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-02-23 17:17:51 +0000

    correction tests

--- compilation/hight/tests/SymbolTableTest.java
+++ compilation/hight/tests/SymbolTableTest.java
@@ -7,13 +7,15 @@ import junit.framework.TestCase;
 
 public class SymbolTableTest extends TestCase {
 SymbolTable s1;
+Symbol t1;
 AttributeValue a1;
 	protected void setUp() throws Exception {
 		super.setUp();
 		SymbolTable s1 = new SymbolTable();
 		AttributeValue a1 = new AttributeValue(3);
+		//Symbol t1 = new Symbol();
 	}
 public void testGet(){
-	//s1.add("abc");
+	s1.add("abc",t1);
 }
 }

bc456dcaf44ff87a22a0aadd61c453cde0c76c74
mathieuguichaoua <mathieuguichaoua@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-03-05 18:33:48 +0000

    suite tests

---
+++ compilation/hight/tests/AttributeEnumTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import junit.framework.TestCase;
+
+public class AttributeEnumTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+public void testSet(){
+	assertEquals(true,true);
+}
+}

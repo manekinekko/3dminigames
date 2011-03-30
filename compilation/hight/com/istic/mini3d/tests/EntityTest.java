32c5ab99edcb57c38c35eb46914eb6c1f7513929
thibault.lecorronc <thibault.lecorronc@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-03-30 16:01:31 +0000

    Suite tests

--- compilation/hight/com/istic/mini3d/tests/EntityTest.java
+++ compilation/hight/com/istic/mini3d/tests/EntityTest.java
@@ -9,19 +9,27 @@ public class EntityTest extends TestCase {
 	Entity entity;
 	Model m, m2, m3;
 	List<Model> models;
-	List<String> liste;
+	List<String> liste, liste2;
 	Map<String, AttributeValue> attributes;
 	AttributeValue val, val2;
 	protected void setUp() throws Exception {
 		super.setUp();
 		entity = new Entity("Entite", m);
 		models = new ArrayList<Model>();
+		liste2 = new ArrayList<String>();
 		liste = new ArrayList<String>();
         attributes = new HashMap<String, AttributeValue>();
         val = new AttributeValue(true);
 		val2 = new AttributeValue(666);
+		m = new Model("mod1");
+		m2 = new Model("mod3");
+		m.addAttribute("att1", val);
+		m2.addAttribute("att2", val2);	
 	}
 	
+	/*
+	 * Passer les attributs de Entity en public pour lancer ces tests.
+	 * 
 	public void testAddAttribute(){
 		attributes.put("att1", val);
 		attributes.put("att2", val2);
@@ -38,11 +46,17 @@ public class EntityTest extends TestCase {
 	}
 	
 	public void testListAttributes(){
-		entity.addAttribute("att1", val);
-		entity.addAttribute("att2", val2);
+		entity = new Entity("testList", m, m2);
+		entity.listAttributes();
 		liste.add("att1"); liste.add("att2");
-		System.out.println(entity.listAttributes());
-		//assertEquals(entity.listAttributes(), liste);
+		liste2.add("att2"); liste2.add("att1");
+		assertEquals(entity.listAttributes(), liste);
+		assertNotSame(entity.listAttributes(), liste2);
 	}
 
+	// Quel est l interet de cette fonction ?
+	public void testListModifyAttributes(){
+		entity = new Entity("testList", m, m2);
+		entity.listModifyAttributes();
+	} */
 }

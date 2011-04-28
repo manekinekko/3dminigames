de6ee37f53a9a681526d42f7969eabb52c24f3d3
wassim.chegham <wassim.chegham@1d3cb33b-0afe-385b-02f0-028d385e7d15>
2011-04-28 17:53:08 +0000

    reorganisation de la partie IHM

---
+++ trunk/dev/IHM/assets/js/lib/codepress/languages/edwigs.js
@@ -0,0 +1,254 @@
+/*
+ * CodePress regular expressions for edwigs syntax highlighting
+ */
+
+// edwigs
+Language.syntax = [
+
+	// numbers
+	{
+		input: /\b([\d]+)\b/g,
+		output: '<em>$1</em>'
+	},
+	
+	// strings double quote
+	{
+		input: /\"(.*?)(\"|<br>|<\/P>)/g,
+		output: '<span>"$1$2</span>'
+	}, 
+	
+	// strings single quote
+	{
+		input: /\'(.*?)(\'|<br>|<\/P>)/g,
+		output: '<span>\'$1$2</span>'
+	}, 
+	
+	// reserved words
+	{
+		input: /\b(Empty|Camera|Zone|Lamp|Ground|Room|Obstacle|Object|Character|Projectile|Weapon|Sword|Vehicle|Craft|Container|Ball|Teleporter|Breakable|Inventory|Counter|Chrono|Media|Background|Side|Team|Player|List)\b/g,
+		output: '<b>$1</b>'
+	}, 
+
+	//mots non-répertoriés : ranking  gravity  name  world  generic  grid  ribbon  gridsize  position  orientation  size
+	//game ne fait plus partie des classes et est un mot réservé
+	{ 
+	  input : /\b(accelerates|activate|add|ai|alive|all|and|angle|anticlockwise|assign|at|backward|becomes|between|brakes|by|clockwise|command|commands|contains|dead|defeat|definition|dies|disable|distance|down|duplicable|during|efface|effaced|else|endif|ends|endwait|enter|equip|escape|expels|finished|first|for|forward|frame|frames|free|from|game|generate|grasps|generated|gets|happens|has|held|if|in|ingests|invert|is|jumps|key|keyboard|killed|kills|last|lclick|left|list|loop|mclick|means|min|mouse|moves|moving|ms|mute|muted|next|nexturn|not|num|of|off|on|or|other|pause|paused|person|play|played|pressed|previous|random|rclick|released|remove|right|rule|s|save|scrollDown|scrollUp|solo|space|started|starts|stop|stopped|sub|then|third|to|touches|touching|turns|turnbased|type|up|until|victory|wait|waiting|with)\b/g, 
+	  output : '<u>$1</u>' 
+	},
+	
+	// comments of type //
+	{
+		input: /([^:]|^)\/\/(.*?)(<br|<\/P)/g,
+		output: '$1<i>//$2</i>$3'
+	}, 
+	
+	// comments of type /* */
+	{ 
+		input : /\/\*(.*?)\*\//g, 
+		output : '<i>/*$1*/</i>' 
+	} 
+];
+
+Language.snippets = [
+
+	//   1. Partie Description du jeu
+	
+	{
+		input: 'game',
+		output: 'game has $0 at    ;\n\n'
+	},
+
+	//   2. Partie Déclaration de nouveaux types
+	
+	// le nouveau type sera forcément de type Object
+	{
+		input: 'type',
+		output: 'type $0 is    ;\n\n'
+	}, 
+	{
+		input: 'typea',
+		output: 'type $0 is    and     ;\n\n'
+	},
+	{
+		input: 'typeaa',
+		output: 'type $0 is    and    and    ;\n\n'
+	},
+
+	//   3. Partie Initialisations
+	
+	// ce has est différencié de l'autre car pour l'autre on appuie sur espace et pas tab
+	{
+		input: 'has',
+		output: 'has $0 at    ;\n\n'
+	},
+	
+	//on a pas l'habitude de taper un mot qui arrivera après (is)
+	{
+		input: 'is',
+		output: '$0 is /*type*/ ;\n\n'
+	},
+	{
+		input: 'isd',
+		output: 'is $0 duplicable ;\n\n'
+	},
+	{
+		input: 'iso',
+		output: 'is Object ;\n\n$0'
+	},
+	{
+		input: 'ist',
+		output: 'is Team ;\n\n$0'
+	},
+	{
+		input: 'isp',
+		output: 'is Player ;\n\n$0'
+	},
+	{
+		input: 'iscam',
+		output: 'is Camera ;\n\n$0'
+	},
+
+	//   4. Partie Définitions;
+	
+	{
+		input: 'def',
+		output: 'definition $0 means\n  // definition body here...\n;\n\n'
+	},
+	{
+		input: 'if',
+		output: 'if $0 then\n  // your actions here...\nendif ;\n'
+	},
+	{
+		input: 'ifelse',
+		output: 'if $0 then\n  // your actions here...\nelse\n  // your actions here...\nendif ;\n\n'
+	},
+	{
+		input: 'wait',
+		output: 'wait $0 s then \n  // your actions here...\nendwait ;\n\n'
+	},
+
+	{
+		input: 'acc',
+		output: 'accelerates by $0 ;\n\n'
+	},
+	{
+		input: 'brk',
+		output: 'brakes by $0 ;\n\n'
+	},
+
+	{
+		input: 'add',
+		output: 'add $0 to    ;\n\n'
+	},
+	{
+		input: 'sub',
+		output: 'sub $0 to    ;\n\n'
+	},
+	{
+		input: 'assign',
+		output: 'assign $0 to    ;\n\n'
+	},
+	{
+		input: 'invert',
+		output: 'invert $0 and    ;\n\n'
+	},
+
+	{
+		input: 'rand',
+		output: 'random between $0 and    '
+	},
+	{
+		input: 'dist',
+		output: 'distance between $0 and    '
+	},
+	{
+		input: 'angle',
+		output: 'angle between $0 and    '
+	},
+
+	{
+		input: 'during',
+		output: 'during $0 s'
+	},
+	{
+		input: 'until',
+		output: 'until $0 happens'
+	},
+
+	{
+		input: 'v',
+		output: 'victory of $0'
+	},
+	{
+		input: 'd',
+		output: 'defeat of $0'
+	},
+
+	//   5. Partie Commandes
+	
+	{
+		input: 'cmd',
+		output: 'command for $0 is\n  // your commands here...\n;\n\n'
+	},
+
+	//   6. Partie Règles du jeu
+	
+	{
+		input: 'rule',
+		output: 'rule $0 then \n  // rule action here...\n;\n\n'
+	},
+
+	//   7. Partie Intelligence Artificielle 
+	
+	{
+		input: 'ai',
+		output: 'ai $0 is \n // your rules here... \n;\n\n'
+	},
+
+	//   8. mots uniques
+	
+	{
+		input: 'bw',
+		output: 'backward $0'
+	},
+	{
+		input: 'fw',
+		output: 'forward $0'
+	},
+	{
+		input: 'cw',
+		output: 'clockwise $0'
+	},
+	{
+		input: 'acw',
+		output: 'anticlockwise $0'
+	},
+	{
+		input: 'cmds',
+		output: 'commands $0'
+	},
+	{
+		input: 'end',
+		output: 'game ends ;\n\n$0'
+	},
+
+	{
+		input: 'gen',
+		output: 'generate $0'
+	},
+	{ 
+		input : 'eff', 
+		output : 'efface $0'
+	}
+];
+
+Language.complete = [
+	{ input : '\'',output : '\'$0\'' },
+	{ input : '"', output : '"$0"' },
+	{ input : '(', output : '\($0\)' },
+	{ input : '[', output : '\[$0\]' },
+	{ input : '<', output : '\<$0\>' },
+	{ input : '{', output : '{\n\t$0\n}' }
+];
+
+Language.shortcuts = [];
---
+++ trunk/dev/IHM/assets/js/m3d_common.js
@@ -0,0 +1,132 @@
+/**
+ * @author CHEGHAM Wassim <wassim.chegham@gmail.com>
+ * @namespace M3D.Common
+ * @file assets/js/m3d_common.js
+ * @projectDescription This file includes some convenient functions.
+ */
+(function(M3D){
+
+	/**
+	 * Global constants
+	 */ 
+	M3D.REGEX_ALPHANUMERIC = /^[A-z0-9_\-]+$/g;
+	M3D.REGEX_NUMBER = /^[0-9]+$/g;
+	
+	/**
+	 * Print a log text. This function taks an infinite number of parameters.
+	 * @param {Mixed} An unlimited number of parameters
+	 */
+    M3D.Common.log = log = function(){
+      if (console) {
+        var script = "console.info('[3DWIGS]', arguments[0]";
+				for(var i=1;i<arguments.length;i++){ script += ",arguments["+i+"]"; }
+				script += ")";
+				
+				// the eval function is very very bad!! 
+				// but hoppefully it must be disabled during prod cycle!!
+				eval(script);
+			}
+      else {
+        alert('console.log() was not found! Please disable the log feature.');
+			}
+    };
+    
+    /**
+     * A convenient function that prints out a select propety of a select box.
+     * @param {String} a The first value (from the select box)
+     * @param {String} b The second value (that should be selected)
+     * @return selected='true' or empty string
+     * @type {string}
+     */
+	M3D.Common.printSelected = function(a, b){
+        return (a === b) ? "selected='true'" : "";
+    };
+    
+	/**
+	 * This function checks if a given type is a valid one.
+	 * @param {String} t The type that must be checked
+	 * @return True if t is a valid type, False if not
+	 * @return {Boolean}
+	 */
+    M3D.Common.isValidType = function(t){
+        var i;
+        var arr = ['string', 'number', 'boolean'];
+        for (i in arr) {
+            if (typeof(t) === arr[i]) {
+                return true;
+			}
+        }
+        return false;
+    };
+	
+	/**
+	 * Checks if a given value is empty
+	 * @param {String} val The value that must be checked
+	 * @return True if val is empty, False if not
+	 * @type {Boolean}
+	 */
+	M3D.Common.isEmpty = function(val){
+		return typeof(val) === 'string' && val === '';
+	};
+	
+	/**
+	 * Checks if a given value matches an alphanumeric pattern.
+	 * @param {String} val The value that must be checked
+	 * @return True if val is an alphanumeric value, False if not
+	 * @type {Boolean}
+	 */
+	M3D.Common.isAlphanumeric = function(val){
+		return M3D.REGEX_ALPHANUMERIC.test(val);
+	};
+	
+	/**
+	 * Checks if a given value matches a number pattern.
+	 * @param {String} val The value that must be cheked
+	 * @return True if val is a number, False if not
+	 * @type {Boolean}
+	 */
+	M3D.Common.isNumber = function(val){
+		return M3D.REGEX_NUMBER.test(val);
+	};
+	
+	/**
+	 * Upper case the first letter of a given string.
+	 * @param {String} value The value that must be a first letter upper cased
+	 * @return The same value with the first letter upper cased
+	 * @type {String}
+	 */
+	M3D.Common.ucfirst = function(value){
+		
+		if (typeof(value) !== "string" || value === "") {
+			return "";
+		}
+		var _first = value.substring(0,1).toUpperCase();
+		return _first+value.substring(1);
+	};
+	
+	/**
+	 * Get the object's ID given its UID
+	 * @param {String} The Object's UID
+	 * @return The Object's ID uppercased
+	 * @type {String} 
+	 */
+	M3D.Common.getObjectId = function(uid){
+		return M3D.Common.ucfirst($('#select-model option[value="'+uid+'"]').text());
+	};
+	
+	/**
+	 * Minimal implementation of the sprintf function of PHP
+	 * @param {Array} Unlimited String parameters where the 1st is the format.
+	 * The format string is composed of zero or more directives
+	 * @return The string produced according to the formatting string format
+	 */
+	M3D.Common.sprintf = function () {
+	    var _str = arguments[0];
+		for(var i=1; i<arguments.length; i++){
+			_str = _str.replace('%s', arguments[i])
+		}
+		return _str;
+	}
+	
+    
+})(window.M3D);
